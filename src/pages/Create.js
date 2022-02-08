3f4dccef834c48ab59889ee0da879a58084a844a
Nadia-Hamid <hamid.nadia@gmail.com>
2022-02-08 09:23:28 +0100

    Buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,28 +1,36 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { ButtonGroup } from "@material-ui/core";
 
 export default function Create() {
   return (
     <div>
-      {/* <Typography 
-      variant="h1"
-      color="primary"
-      align="center"
-       >
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
         Create a new note
-      </Typography> */}
-      {/* <Typography 
-      color="secondary"
-      noWrap="true">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et
-        arcu maximus, sagittis quam in, condimentum risus. Nunc faucibus, nibh
-        vel ornare viverra, erat elit faucibus nisi, et scelerisque dui nisi a
-        leo. Vestibulum et semper est. Aliquam erat volutpat. Nullam a lectus
-        dui. Ut nisi nisi, elementum ut nunc ac, pretium sagittis urna. Nulla
-        facilisi. Aenean laoreet auctor odio, faucibus aliquam sem venenatis
-        vel.
-      </Typography> */}
-      <Typography variant="h6" color="textSecondary" component ="h2" gutterBottom>Create a new note</Typography>
+      </Typography>
+      <Button type="submit" color="primary">
+        Submit
+      </Button>
+      <Button type="submit" color="secondary" variant="outlined">
+        Submit
+      </Button>
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+        <Button>Four</Button>
+      </ButtonGroup>
+      <Button 
+      onClick={() => console.log("you clicked me")}
+      type="submit" color="secondary" variante="contained">
+        Submit
+      </Button>
     </div>
   );
 }
